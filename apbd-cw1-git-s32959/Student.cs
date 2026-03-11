2c4e1451e01bead81aadea7b409cf0256c547422
Robert Gawroński <s32959@pjwstk.edu.pl>
2026-03-11 11:42:15 +0100

    add Student class

---
+++ apbd-cw1-git-s32959/Student.cs
@@ -0,0 +1,15 @@
+namespace apbd_cw1_git_s32959;
+
+public class Student
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string SNumber { get; }
+
+    public Student(string name, string surname, string sNumber)
+    {
+        this.Name = name;
+        this.Surname = surname;
+        this.SNumber = sNumber;
+    }
+}
