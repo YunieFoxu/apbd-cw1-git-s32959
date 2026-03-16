ee0c7573636c59ca074e86195a5abd33743ca8a6
Robert Gawroński <s32959@pjwstk.edu.pl>
2026-03-16 21:14:32 +0100

    add StatisticsHelper class

---
+++ apbd-cw1-git-s32959/StatisticsHelper.cs
@@ -0,0 +1,24 @@
+﻿namespace apbd_cw1_git_s32959;
+
+public class StatisticsHelper
+{
+    private List<int> _statistics;
+
+    public StatisticsHelper()
+    {
+        _statistics = new List<int>();
+    }
+
+    public void AddStatistic(int statistic)
+    {
+        _statistics.Add(statistic);
+    }
+
+    public void ListStatistics()
+    {
+        foreach (int x in _statistics)
+        {
+            Console.Write(x + " ");
+        }
+    }
+}
