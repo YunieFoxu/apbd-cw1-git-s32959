dbacfd143e4092ddf175a1308dfd06167369ba28
Robert Gawroński <s32959@pjwstk.edu.pl>
2026-03-17 01:39:16 +0100

    add more tests

--- apbd-cw1-git-s32959/Program.cs
+++ apbd-cw1-git-s32959/Program.cs
@@ -3,5 +3,9 @@
 StatisticsHelper test = new StatisticsHelper(new []{5, 1, 2, 10, 144, 3, 5, 2, 9});
 
 test.ListStatisticsInOrder();
-if (test.CalculateMax()!=144) Console.WriteLine("Max test failed");
-if (test.CalculateMin()!=1) Console.WriteLine("Min test failed");
+if (test.CalculateMax()!=144) 
+    Console.WriteLine("Max test failed");
+if (test.CalculateMin()!=1) 
+    Console.WriteLine("Min test failed");
+if (!test.CalculateAverage().Equals(new []{5, 1, 2, 10, 144, 3, 5, 2, 9}.Average()))
+    Console.WriteLine("Average test failed");
